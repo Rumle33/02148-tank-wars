19bbe4e81cc20c901c9dd66c4c197319c9a27835
Rumle Kjaer Ulsø <rumlekjaerulso@mbptilhnderumle.students.clients.local>
2025-01-09 09:44:33 +0100

    javafx working

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,92 +1,19 @@
-// This is a model of the classic problem of the dining philosophers.
-// The solution is based on using tickets to limit concurrency.
-
 package org.example;
 
-import org.jspace.ActualField;
-import org.jspace.SequentialSpace;
-import org.jspace.Space;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class Main {
-    // N defines the number of philosophers.
-    public static final int N = 10;
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX App");
+        primaryStage.setScene(new Scene(new Label("Hello JavaFX"), 400, 300));
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-        Space board = new SequentialSpace();
-
-        new Thread(new waiter(board)).start();
-
-        for (int i = 0; i < N; i ++) {
-            new Thread(new philosopher(board, i)).start();
-        }
-
-        try {
-            board.query(new ActualField("done")); // will never succeed
-        } catch (InterruptedException e) {}
-    }
-}
-
-// waiter prepares the board with forks.
-class waiter implements Runnable {
-    private Space board;
-
-    public waiter(Space space) {
-        this.board = space;
-    }
-
-    public void run() {
-        System.out.println("Waiter putting forks on the table...");
-
-        for (int i = 0; i < Main.N; i ++) {
-            try {
-                board.put("fork", i);
-                System.out.println("Waiter put fork " + i + " on the table.");
-            } catch (InterruptedException e) {}
-        }
-
-        System.out.println("Waiter done.");
-    }
-}
-
-
-
-// philosopher defines the behaviour of a philosopher.
-class philosopher implements Runnable {
-    private Space board;
-    private int me, left, right;
-
-    public philosopher(Space space, int me) {
-        this.board = space;
-
-        // We define variables to identify the left and right forks.
-        this.me = me;
-        this.left = me;
-        this.right = (me + 1) % Main.N;
-    }
-
-    public void run() {
-
-        // The philosopher enters his endless life cycle.
-        while (true) {
-            try {
-
-                // Wait until the left fork is ready (get the corresponding tuple).
-                board.get(new ActualField("fork"), new ActualField(left));
-                System.out.println("Philosopher " + me + " got left fork");
-
-                // Wait until the right fork is ready (get the corresponding tuple).
-                board.get(new ActualField("fork"), new ActualField(right));
-                System.out.println("Philosopher " + me + " got right fork");
-
-                // Lunch time.
-                System.out.println("Philosopher " + me + " is eating...");
-
-                // Return the forks (put the corresponding tuples).
-                board.put("fork", left);
-                board.put("fork", right);
-                System.out.println("Philosopher " + me + " put both forks on the table");
-
-            } catch (InterruptedException e) {}
-        }
+        launch(args);
     }
 }
