84b9c030513021675c51ba1b84a2f02f145b6eff
Magnus <88041689+MagnusAH@users.noreply.github.com>
2025-01-09 10:35:29 +0100

    simulation base prototype

---
+++ src/main/java/org/example/server/Simulation.java
@@ -0,0 +1,33 @@
+package org.example.server;
+
+public class Simulation {
+
+	public static final int UPS = 50;
+
+	public void run() {
+
+		long lastTime = System.currentTimeMillis();
+
+		while (true) {
+			long currentTime = System.currentTimeMillis();
+
+			while (lastTime - currentTime < 1000 / Server.UPS) {
+				Thread.sleep(1);
+			}
+
+			long deltaTime = currentTime - lastTime;
+			double delta = ((double)deltaTime * UPS) / 1000.0 
+
+			// do update
+
+			
+
+			// end of update
+
+			lastTime = currentTime;
+		}
+
+
+	}	
+
+}
