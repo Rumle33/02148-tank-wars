1371844e2459082b2cf43fabdc77c00ff1a0aa64
Abinav Reddy Aleti <s224786@dtu.dk>
2025-01-10 11:24:59 +0100

    added map generation

--- src/main/java/org/example/Tank/Main.java
+++ src/main/java/org/example/Tank/Main.java
@@ -1,9 +1,10 @@
 package org.example.Tank;
 
 import javafx.application.Application;
+import org.example.Maps.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Application.launch(Tank.class, args);
+        Application.launch(Map.class, args);
     }
 }
