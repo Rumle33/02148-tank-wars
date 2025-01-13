87b47dadc7391d355bdd05c9169f683480289879
Magnus <88041689+MagnusAH@users.noreply.github.com>
2025-01-13 12:54:56 +0100

    remove bad

--- src/main/java/org/example/util/MathUtil.java
+++ src/main/java/org/example/util/MathUtil.java
@@ -10,36 +10,4 @@ public class MathUtil {
 		return val < min ? min : val > max ? max : val;
 	}
 
-	public Vector2f collideMM(float[] vertexData0, float[] vertexData1) {
-		for (int i = 0; i < vertexData0.length - 2; i = i + 2) {
-			// get axis to test
-			float axisX = vertexData0[i + 1] - vertexData0[i + 2 + 1];
-			float axisY = vertexData0[i + 2] - vertexData0[i];
-
-			// normalize axis
-			{
-				float nfactor = 1.0f / (float) Math.sqrt(axisX * axisX + axisY * axisY);
-				axisX *= nfactor;
-				axisY *= nfactor;
-			}
-
-			// project first mesh
-			
-
-
-			// project second mesh
-
-		}
-
-		return null;
-	}
-
-	public Vector2f collideMC(float[] vertexData0, float x, float y, float radius) {
-		for (int i = 0; i < vertexData0.length >> 1; i++) {
-
-		}
-
-		return null;
-	}
-
 }
