002de90443021e5dc1f566df980f49684f61b309
Magnus <88041689+MagnusAH@users.noreply.github.com>
2025-01-13 12:53:51 +0100

    more math utils

---
+++ src/main/java/org/example/util/Vector3f.java
@@ -0,0 +1,29 @@
+package org.example.util;
+
+public class Vector3f {
+	
+	public float x;
+	public float y;
+	public float z;
+
+	public Vector3f(Vector3f v) {
+		this.x = v.x;
+		this.y = v.y;
+		this.z = v.z;
+	}
+
+	public Vector3f(Vector2f v, float z) {
+		this.x = v.x;
+		this.y = v.y;
+		this.z = z;
+	}
+
+	public Vector3f(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	
+
+}
